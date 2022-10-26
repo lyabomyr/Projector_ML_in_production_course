be738e2c36a8f710ea0a656fa361d60c06150f85
lyabomyr <68542310+lyabomyr@users.noreply.github.com>
2022-10-27 02:15:01 +0300

    Week5 (#26)
    week 5

--- week5/serving/fast_api.py
+++ week5/serving/fast_api.py
@@ -3,15 +3,16 @@ from fastapi.responses import StreamingResponse
 import pandas as pd
 import os
 from io import BytesIO, StringIO
-from serving.ml_decision_tree_sample.prepare_data import prepare_data_for_train, common_prepare, prepare_data_for_predict
+from serving.ml_decision_tree_sample.prepare_data import prepare_data_for_train, common_prepare, \
+    prepare_data_for_predict
 from serving.ml_decision_tree_sample.predictor import model_for_predict
 from serving.ml_decision_tree_sample.load_model import model_loader
 from serving.ml_decision_tree_sample.train import grid_train_model
 from serving.ml_decision_tree_sample.cleaner import clean_all
 from serving.config import model_file_name
 import uvicorn
-
-
+from pydantic import BaseModel
+from fastapi.responses import FileResponse
 
 
 def request_csv_to_df(file: UploadFile) -> pd.DataFrame:
@@ -31,17 +32,26 @@ def return_df_like_csv_file_in_response(df: pd.DataFrame):
     return response
 
 
+class Message(BaseModel):
+    message: str
+
+class Train_mes(BaseModel):
+    text_mes: str
+    model_name: str
+
+
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", responses={404: {"model": Message}})
 def read_root():
-    return {"Hello": "please add \'/docs\' to ur url and u will be transferred to swagger page with list of request "
-                     "example url http://127.0.0.1:8000/docs"}
+    return Message(** {
+        'message': 'please add \'/docs\' to ur url and u will be transferred to swagger page with list of request '
+                   'example url http://127.0.0.1:8000/docs'})
 
 
-@app.post("/train_model_and_push_to_wandb/")
-async def train_model_and_push_to_wandb(file: UploadFile = File(...)):
+@app.post("/train_model_and_push_to_wandb/", response_model=Train_mes)
+def train_model_and_push_to_wandb(file: UploadFile = File(...)):
     if not file:
         return {"message": "No upload file sent"}
     else:
@@ -50,18 +60,17 @@ async def train_model_and_push_to_wandb(file: UploadFile = File(...)):
                                  model_file_name)
         model_loader('upload', model_file_name)
         clean_all(model_file_name)
-        return {"Complete train and sent to wandb with model_name": train}
+        return Train_mes(**{'text_mes': 'Complete train and sent to wandb with model_name',
+                            'model_name': train})
 
 
-@app.post("/batch_predict_csv_file/")
-async def predict_exel_file(file: UploadFile = File(...)):
+@app.post("/batch_predict_csv_file/", response_class=FileResponse)
+def predict_exel_file(file: UploadFile = File(...)):
     if not file:
         return {"message": "No upload file sent"}
     else:
         input_df = request_csv_to_df(file)
         model_loader('download', model_file_name)
-        print(prepare_data_for_predict(input_df))
-        print('sddddddddddddddddd',prepare_data_for_predict(input_df).columns)
         output_df = model_for_predict(prepare_data_for_predict(input_df), model_file_name)
         input_df['Exited'] = output_df
         result = input_df
