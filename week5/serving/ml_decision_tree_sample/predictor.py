998e3ebe54d058f0c24354f7d90b8408b3f7d8d4
lyabomyr <lyabomyr@gmail.com>
2022-11-12 14:38:16 +0200

    fs

--- week5/serving/ml_decision_tree_sample/predictor.py
+++
@@ -1,8 +0,0 @@
-from sklearn.ensemble import RandomForestClassifier
-import pandas as pd
-import joblib
-
-def model_for_predict(X_test: pd.DataFrame, path_to_model_file: str):
-    clf = joblib.load(path_to_model_file)
-    predict = clf.predict(X_test)
-    return pd.DataFrame(predict)
