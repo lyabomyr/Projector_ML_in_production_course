858b71e2a8e6d63ca0ff3815a89299a298799e46
lyabomyr <68542310+lyabomyr@users.noreply.github.com>
2022-11-30 22:51:23 +0200

    Week5 (#24) (#25)
    * Week5 (#24)
    
    * week5 seldon, fastapi, streamlit
    
    * Week5 (#26)
    
    week 5
    
    * Week5 hm2 (#27)
    
    * week5 seldon, fastapi, streamlit
    
    * week5 seldon, fastapi, streamlit
    
    * add solden week5
    
    * week5
    
    * fix after PR
    
    * fix after PR
    
    * fix after PR
    
    * fix after PR
    
    * week5 benchmarking
    
    * add result to test bechmark-forwardpass and fix file in week1
    
    * add result to test bechmark-forwardpass and fix file in week1
    
    * add result to test bechmark-forwardpass and fix file in week1
    
    * Update week5.yml
    
    * Update requirements.txt
    
    * Update README.md
    
    * Update week5.yml
    
    * Update week5.yml
    
    * Update Dockerfile
    
    * Update test_fastapi.py
    
    * Update week5.yml
    
    * Update test_fastapi.py
    
    * Update week5.yml
    
    * Week5 hm2 (#28)
    
    * week5 seldon, fastapi, streamlit
    
    * week5 seldon, fastapi, streamlit
    
    * add solden week5
    
    * week5
    
    * fix after PR
    
    * fix after PR
    
    * fix after PR
    
    * fix after PR
    
    * week5 benchmarking

---
+++ week5/serving/ml_decision_tree_sample/cleaner.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+from serving.config import remove_dir_name
+
+
+def remove_model_from_local_directory(model_name: str):
+    if os.path.exists(model_name):
+        os.remove(model_name)
+        print(f"The file {model_name} has been deleted successfully")
+    else:
+        print(f"The file {model_name} does not exist!")
+
+
+def remove_wand_local_directory():
+    if os.path.isdir(remove_dir_name):
+        shutil.rmtree(remove_dir_name)
+    else:
+        print(f'{remove_dir_name} directory does not exist!')
+
+
+def clean_all(model_name: str):
+    remove_model_from_local_directory(model_name)
+    remove_wand_local_directory()
---
+++ week5/unit_tests/test_benchmark_optimization_inference.py
@@ -0,0 +1,59 @@
+from concurrent.futures import wait
+import numpy as np
+import concurrent
+import pandas as pd
+import pytest_benchmark
+from serving.ml_decision_tree_sample.predictor import model_for_predict
+from serving.config import train_file, model_file_name, test_file_for_predict
+from serving.ml_decision_tree_sample.prepare_data import prepare_data_for_train, common_prepare, \
+    prepare_data_for_predict
+from serving.ml_decision_tree_sample.load_model import model_loader
+from serving.ml_decision_tree_sample.train import grid_train_model
+
+
+def download_model():
+    model_loader('download', model_file_name)
+
+
+def my_predict(file):
+    input_df = pd.read_csv(test_file_for_predict)
+    output_df = model_for_predict(prepare_data_for_predict(input_df), model_file_name)
+    input_df['Exited'] = output_df
+    result = input_df
+    return result
+
+
+def run_inference(x_test=prepare_data_for_predict(pd.read_csv(test_file_for_predict)), batch_size: int = 1500):
+    y_pred = []
+    for i in range(0, x_test.shape[0], batch_size):
+        x_batch = x_test[i: i + batch_size]
+        y_batch = model_for_predict(x_batch, model_file_name)
+        y_pred.append(y_batch)
+    return np.concatenate(y_pred)
+
+
+def run_inference_process_pool(X=prepare_data_for_predict(pd.read_csv(test_file_for_predict)), max_workers: int = 14):
+    with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
+        chunk_size = len(X) // max_workers
+        chunks = []
+        for i in range(0, len(X), chunk_size):
+            chunks.append(X[i: i + chunk_size])
+        futures = []
+        for chunk in chunks:
+            future = executor.submit(run_inference, x_test=chunk)
+            futures.append(future)
+        wait(futures)
+        y_pred = []
+        for future in futures:
+            y_batch = future.result()
+            y_pred.append(y_batch)
+    return np.concatenate(y_pred)
+
+def test_optimized_predict(benchmark):
+    benchmark(run_inference_process_pool)
+
+# -------------------------------------------------- benchmark: 1 tests -------------------------------------------------
+# Name (time in ms)               Min       Max      Mean  StdDev    Median     IQR  Outliers     OPS  Rounds  Iterations
+# -----------------------------------------------------------------------------------------------------------------------
+# test_optimized_predict     162.6492  179.1311  170.1867  5.9578  170.0330  8.3828       2;0  5.8759       6           1
+# -----------------------------------------------------------------------------------------------------------------------
